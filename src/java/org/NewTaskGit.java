256907a6014097671a994d83f0f7faf3118f1005
jansi1989 <antojanu@gmail.com>
2020-05-04 22:57:10 +0530

    new code added in rani

--- src/java/org/NewTaskGit.java
+++ src/java/org/NewTaskGit.java
@@ -2,7 +2,9 @@ package java.org;
 
 public class NewTaskGit {
 	public static void main(String[] args) {
-		
+		System.out.println("rani");
+		System.out.println("jansi");
+		System.out.println("vv");
 	}
 
 
